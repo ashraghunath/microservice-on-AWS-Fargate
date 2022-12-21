5e48507fd16e50fb26bb816c394a49657a3af98b
Ashwin Raghunath <ashwinr2296@gmail.com>
2022-12-21 17:20:57 -0500

    Microservices added

---
+++ currency-exchange-service-mysql/src/main/java/com/ashwin/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationMySql.java
@@ -0,0 +1,34 @@
+package com.ashwin.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import com.ashwin.microservices.currencyexchangeservice.resource.ExchangeValue;
+import com.ashwin.microservices.currencyexchangeservice.resource.ExchangeValueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeServiceApplicationMySql implements CommandLineRunner {
+
+	@Autowired
+    ExchangeValueRepository repository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplicationMySql.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		long count = repository.count();
+
+		if (count == 0) {
+			repository.save(new ExchangeValue(10001L, "USD", "INR", BigDecimal.valueOf(60)));
+			repository.save(new ExchangeValue(10002L, "EUR", "INR", BigDecimal.valueOf(70)));
+			repository.save(new ExchangeValue(10003L, "AUD", "INR", BigDecimal.valueOf(20)));
+		}
+	}
+
+}
